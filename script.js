cd8424c60a61b83b89b11255ceb1f9b5a67ed0f5
PŮČEK Pavel <pavel.pucek@pcr.cz>
2025-04-25 07:49:13 +0200

    Sjednocena a opravena verze index.html

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const supabaseUrl = 'https://lmcmwrehrmgygsnyofdf.supabase.co';
-const supabaseKey = 'eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtY213cmVocm1neWdzbnlvZmRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjMzNzIsImV4cCI6MjA2MDgzOTM3Mn0.oJ7rRKOg2FAurmZanqvanyu4k0gnEXQfKawayZHeSBQ'; // ← zde celý klíč
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+const client = window.supabaseClient;
 const select = document.getElementById('disciplineSelect');
 const formContainer = document.getElementById('formContainer');
 const resultsContainer = document.getElementById('resultsContainer');
