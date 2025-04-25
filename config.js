5dd5c30bd2a4de7f711674bbb95f2e5f1d7dd77c
PŮČEK Pavel <pavel.pucek@pcr.cz>
2025-04-25 08:20:55 +0200

    snad už funkční připojení k databázi

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-
 const SUPABASE_URL = 'https://lmcmwrehrmgygsnyofdf.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtY213cmVocm1neWdzbnlvZmRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjMzNzIsImV4cCI6MjA2MDgzOTM3Mn0.oJ7rRKOg2FAurmZanqvanyu4k0gnEXQfKawayZHeSBQ';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-window.supabaseClient = supabase;
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
